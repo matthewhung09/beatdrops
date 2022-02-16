c66076455ed00ed4eb7edbb2f0b53b66cdba80fe
Adelle Vo <adelle.vo@gmail.com>
2022-02-16 13:01:27 -0800

    password

--- frontend/src/components/SignUpForm/SignUpForm.js
+++ frontend/src/components/SignUpForm/SignUpForm.js
@@ -1,10 +1,12 @@
 import { useState, React} from "react";
-import { TextField, Button, StylesProvider } from "@material-ui/core";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 import styled from "styled-components";
-import Box from "@material-ui/core/Box";
+import { TextField, InputAdornment, IconButton, Box, Button, StylesProvider } from "@material-ui/core";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+
 import isEmailValidator from 'validator/lib/isEmail';
 import Popup from 'reactjs-popup';
 import '../../App.css';
@@ -79,14 +81,19 @@ function SignUpForm() {
   
     // info for required entries
     const rEntries = [
-      { name: "email", label: "Email" },
-      { name: "password", label: "Create a password" },
-      { name: "username", label: "What should we call you?" },
+      { input: "email", label: "Email" },
+      { input: "password", label: "Create a password" },
+      { input: "username", label: "What should we call you?" },
     ];
 
     // sets popup to be open when page is first loaded
     const [open, setOpen] = useState(true);
     const closeModal = () => setOpen(false);
+
+    // set password visibility
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => setShowPassword(!showPassword);
+    const handleMouseDownPassword = () => setShowPassword(!showPassword);
 
     return (
       <StylesProvider injectFirst>
@@ -99,43 +106,68 @@ function SignUpForm() {
                 <PopupTitle> Sign up for a free beatdrops account.</PopupTitle>
                 <form style={{width: "100%"}} onSubmit={handleSubmit(onSubmit)}>
                     {rEntries.map((entry) => (
-                    <div>
-                        <Controller
-                          key={entry.name}
-                          name={entry.name}
-                          defaultValue=""
-                          control={control}
-                          render={({
-                              field: { onChange, value },
-                              fieldState: { error },
-                          }) => (
-                              <Box m={2}>
-                                <TextField 
-                                    key={entry.name + "a;sldkf"}
-                                    fullWidth
-                                    label={entry.label}
-                                    variant={variant}
-                                    value={value}
-                                    onChange={onChange}
-                                    error={!!error}
-                                    helperText={error ? error.message : null}
+                      <Controller defaultValue="" key={entry.input}
+                        name={entry.input} // unique name of your input
+                        control={control} // control obj from invoking useForm
+                        // render prop: technique for sharing code between React components using a prop whose value is a function
+                        render={({
+                            field: { 
+                              // value, // curr val of controlled component
+                              name, // input's name being registered
+                              onChange // sends input's val to library
+                            },
+                            fieldState: { 
+                              error // err for this specific input
+                            }, 
+                        }) => (
+                            <Box m={2}>
+                              {entry.input === "password" ? (
+                                <TextField fullWidth key={name}
+                                  variant={variant}
+                                  label={entry.label}
+                                  onChange={onChange}
+                                  error={!!error}
+                                  helperText={error ? error.message : null}
+                                  type={showPassword ? "text" : "password"}
+                                  InputProps={{ 
+                                    endAdornment: (
+                                      <InputAdornment position="end">
+                                        <IconButton style={{width: 50}}
+                                          aria-label="toggle password visibility"
+                                          onClick={handleClickShowPassword}
+                                          onMouseDown={handleMouseDownPassword}
+                                          edge="end"
+                                        >
+                                          {showPassword ? <Visibility /> : <VisibilityOff />}
+                                        </IconButton>
+                                      </InputAdornment>
+                                    )
+                                  }}
                                 />
-                              </Box>
-                          )}
-                        />
-                    </div>
+                              ) : (
+                                <TextField fullWidth key={name}
+                                  variant={variant}
+                                  label={entry.label}
+                                  onChange={onChange}
+                                  error={!!error}
+                                  helperText={error ? error.message : null}
+                                />
+                              )}
+                            </Box>
+                        )}
+                      />
                     ))}
+                    {/* component for styling */}
                     <Box m={2}
                       display="flex" 
                       flexDirection="column"
                       alignItems="center"
                       justifyContent="center"
                     >
-                      <StyledButton
+                      <StyledButton fullWidth
                         type="submit"
-                        variant="contained"
-                        color="primary"
-                        fullWidth
+                        variant="contained" 
+                        color="primary" //  
                       >
                         Continue
                       </StyledButton>
