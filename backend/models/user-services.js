52d9d48558004ebc396599641a57e88ee3876256
Matthew Hung <mafewh119@gmail.com>
2022-02-23 15:49:21 -0800

    'oops'

--- backend/models/user-services.js
+++ backend/models/user-services.js
@@ -16,7 +16,7 @@ function getDbConnection() {
 async function addUser(user){
     const userModel = getDbConnection().model("User", UserSchema);
     try{
-        const userToAdd = new userModel(post);
+        const userToAdd = new userModel(user);
         const savedUser = await userToAdd.save()
         return savedUser;
     }catch(error) {
