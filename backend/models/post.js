bba2a20901bb5179cd560ebde77e52a241c4c6f2
Adelle Vo <adelle.vo@gmail.com>
2022-02-28 14:15:41 -0800

    actually commit

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,26 +1,26 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  artist: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  likes: {type: Number},
-  position: {
-    longitude: Number,
-    latitude: Number,
-  },
-  url: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-}, {collection : 'Posts', timestamps: true});
+const PostSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        artist: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        likes: { type: Number },
+        location: { type: String },
+        url: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+    },
+    { collection: "Posts", timestamps: true }
+);
 
 module.exports = PostSchema;
