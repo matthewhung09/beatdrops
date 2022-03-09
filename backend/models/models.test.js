819a0ea43574b0b769c8c76b9e52bd439818ef2a
gscotti6950 <griffinscotti@gmail.com>
2022-03-09 14:22:11 -0800

    removed comments

--- backend/models/models.test.js
+++ backend/models/models.test.js
@@ -112,33 +112,6 @@ test("Fetching all users", async () => {
   expect(users.length).toBeGreaterThan(0);
 });
 
-// test("Fetching users by name", async () => {
-//   const userName = "Ted Lasso";
-//   const users = await userServices.getUsers(userName);
-//   expect(users).toBeDefined();
-//   expect(users.length).toBeGreaterThan(0);
-//   users.forEach((user) => expect(user.name).toBe(userName));
-// });
-
-// test("Fetching users by job", async () => {
-//   const userJob = "Soccer coach";
-//   const users = await userServices.getUsers(undefined, userJob);
-//   expect(users).toBeDefined();
-//   expect(users.length).toBeGreaterThan(0);
-//   users.forEach((user) => expect(user.job).toBe(userJob));
-// });
-
-// test("Fetching users by name and job", async () => {
-//   const userName = "Ted Lasso";
-//   const userJob = "Soccer coach";
-//   const users = await userServices.getUsers(userName, userJob);
-//   expect(users).toBeDefined();
-//   expect(users.length).toBeGreaterThan(0);
-//   users.forEach(
-//     (user) => expect(user.name).toBe(userName) && expect(user.job).toBe(userJob)
-//   );
-// });
-
 test("Fetching by invalid id format", async () => {
   const anyId = "123";
   const user = await userServices.findUserById(anyId);
@@ -166,25 +139,6 @@ test("Fetching by valid id and finding", async () => {
   expect(foundUser.name).toBe(addedUser.name);
   expect(foundUser.email).toBe(addedUser.email);
 });
-
-// test("Deleting a user by Id -- successful path", async () => {
-//   const dummyUser = {
-//     username: "Griffin",
-//     password: "DogFan4571?",
-//     email: "gMan@gmail.com",
-//     liked: []
-//   };
-//   const result = new userModel(dummyUser);
-//   const addedUser = await result.save();
-//   const deleteResult = await userModel.findOneAndDelete({ _id: addedUser.id });
-//   expect(deleteResult).toBeTruthy();
-// });
-
-// test("Deleting a user by Id -- inexisting id", async () => {
-//   const anyId = "6132b9d47cefd0cc1916b6a9";
-//   const deleteResult = await userModel.findOneAndDelete({ _id: anyId });
-//   expect(deleteResult).toBeNull();
-// });
 
 test("Adding user -- successful path", async () => {
   const dummyUser = {
@@ -275,21 +229,5 @@ test("Adding user -- successful path", async () => {
 //     liked: []
 //   };
 //   const result = await userServices.addUser(anotherDummyUser);
-//   expect(result).toBeFalsy();
-// });
-
-// test("Adding user -- failure path with no job", async () => {
-//   const dummyUser = {
-//     name: "Harry Potter",
-//   };
-//   const result = await userServices.addUser(dummyUser);
-//   expect(result).toBeFalsy();
-// });
-
-// test("Adding user -- failure path with no name", async () => {
-//   const dummyUser = {
-//     job: "Young wizard",
-//   };
-//   const result = await userServices.addUser(dummyUser);
 //   expect(result).toBeFalsy();
 // });
