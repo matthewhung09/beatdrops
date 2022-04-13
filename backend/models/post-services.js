db5172a1fb1d5517d416e0553040c52f7216c877
Matthew Hung <mafewh119@gmail.com>
2022-04-13 15:58:24 -0700

    it works

--- backend/models/post-services.js
+++ backend/models/post-services.js
@@ -1,93 +1,96 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const PostSchema = require("./post");
 
 let dbConnection;
 
-function setConnection(newConn){
-    dbConnection = newConn;
-    return dbConnection;
-  }
+function setConnection(newConn) {
+  dbConnection = newConn;
+  return dbConnection;
+}
 
 function getDbConnection() {
-    if (!dbConnection) {
-      dbConnection = mongoose.createConnection(process.env.CONNECTION_URL, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-      });
-    }
-    return dbConnection;
+  if (!dbConnection) {
+    dbConnection = mongoose.createConnection(process.env.CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   }
-
-  async function getPostsByLocation(lat, long){
-    const postModel = getDbConnection().model("Post", PostSchema);
-    const result = await postModel.find({'location.lat': {$lte: (lat + 0.0145), $gte: (lat - 0.0145)}, 
-                                        'location.long': {$lte: (long + 0.0183), $gte: (long - 0.0183)}});
-    return result;
+  return dbConnection;
 }
 
-  async function getPosts(title, artist){
-    const postModel = getDbConnection().model("Post", PostSchema);
-    let result;
-    if(title === undefined && artist === undefined){
-        result = await postModel.find();
-    }
-    else if(title && !artist){
-        result = findPostByTitle(title);
-    }
-    else if(!title && artist){
-        result = findPostByArtist(artist)
-    }
-    return result;  
+async function getPostsByLocation(lat, long) {
+  const postModel = getDbConnection().model("Post", PostSchema);
+  const result = await postModel.find({
+    "location.lat": { $lte: lat + 0.0145, $gte: lat - 0.0145 },
+    "location.long": { $lte: long + 0.0183, $gte: long - 0.0183 },
+  });
+  return result;
 }
 
-async function addPost(post){
-    const postModel = getDbConnection().model("Post", PostSchema);
-    try{
-        const postToAdd = new postModel(post);
-        const savedPost = await postToAdd.save()
-        return savedPost;
-    }catch(error) {
-        return false;
-    }   
+async function getPosts(title, artist) {
+  const postModel = getDbConnection().model("Post", PostSchema);
+  let result;
+  if (title === undefined && artist === undefined) {
+    result = await postModel.find();
+  } else if (title && !artist) {
+    result = findPostByTitle(title);
+  } else if (!title && artist) {
+    result = findPostByArtist(artist);
+  }
+  return result;
 }
 
-async function updateLikeStatus(id, liked_status){
-    const postModel = getDbConnection().model("Post", PostSchema);
-    if (!liked_status) {
-        return await postModel.findByIdAndUpdate(id, { 
-            $inc: {likes: 1}, 
-            $set: {liked: true}, 
-        },
-            {new: true}
-        );
-    }
-    else {
-        return await postModel.findByIdAndUpdate(id, { 
-            $inc: {likes: -1}, 
-            $set: {liked: false}, 
-        },
-            {new: true}
-        );
-    }
+async function addPost(post) {
+  const postModel = getDbConnection().model("Post", PostSchema);
+  try {
+    const postToAdd = new postModel(post);
+    const savedPost = await postToAdd.save();
+    return savedPost;
+  } catch (error) {
+    return false;
+  }
 }
 
-async function findPostById(id){
-    const postModel = getDbConnection().model("Post", PostSchema);    
-    try{
-        return await postModel.findById(id);
-    }catch(error) {
-        return undefined;
-    }
+async function updateLikeStatus(id, liked_status) {
+  const postModel = getDbConnection().model("Post", PostSchema);
+  if (!liked_status) {
+    return await postModel.findByIdAndUpdate(
+      id,
+      {
+        $inc: { likes: 1 },
+        $set: { liked: true },
+      },
+      { new: true }
+    );
+  } else {
+    return await postModel.findByIdAndUpdate(
+      id,
+      {
+        $inc: { likes: -1 },
+        $set: { liked: false },
+      },
+      { new: true }
+    );
+  }
 }
 
-async function findPostByTitle(title){
-    const postModel = getDbConnection().model("Post", PostSchema);
-    return await postModel.find({'title':title});
+async function findPostById(id) {
+  const postModel = getDbConnection().model("Post", PostSchema);
+  try {
+    return await postModel.findById(id);
+  } catch (error) {
+    return undefined;
+  }
 }
 
-async function findPostByArtist(artist){
-    const postModel = getDbConnection().model("Post", PostSchema);
-    return await postModel.find({'artist':artist});
+async function findPostByTitle(title) {
+  const postModel = getDbConnection().model("Post", PostSchema);
+  return await postModel.find({ title: title });
+}
+
+async function findPostByArtist(artist) {
+  const postModel = getDbConnection().model("Post", PostSchema);
+  return await postModel.find({ artist: artist });
 }
 
 exports.getPosts = getPosts;
--- frontend/src/components/Post/Post.js
+++ frontend/src/components/Post/Post.js
@@ -1,6 +1,10 @@
 import { React, useState, useEffect } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
-import { RiPlayListAddLin, RiPlayListFill, RiUserLocationFill } from "react-icons/ri";
+import {
+  RiPlayListAddLin,
+  RiPlayListFill,
+  RiUserLocationFill,
+} from "react-icons/ri";
 import { MdBookmark, MdBookmarkAdd } from "react-icons/md";
 import { BsBookmarkHeartFill, BsBookmarkHeart } from "react-icons/bs";
 import { TiLocation } from "react-icons/ti";
@@ -20,266 +24,288 @@ import Popup from "reactjs-popup";
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
-    PaperProps: {
-        style: {
-            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-            width: 250,
-        },
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
     },
+  },
 };
 
 const useStyles = makeStyles((theme) => ({
-    quantityRoot: {
-        color: "#FFFFFF",
-        backgroundColor: "#303039",
-        opacity: 0.6,
-        borderRadius: "5px",
-        "&:hover": {
-            backgroundColor: "#1E1E24",
-            borderRadius: "5px",
-            opacity: 1,
-        },
-        "&:focus-within": {
-            backgroundColor: "#1E1E24",
-            borderRadius: "5px",
-            opacity: 1,
-        },
-        "& .MuiOutlinedInput-notchedOutline": {
-            border: "1px solid #484850",
-        },
-        "&:hover .MuiOutlinedInput-notchedOutline": {
-            border: "1px solid #484850",
-        },
-        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-            border: "1px solid #484850",
-            borderRadius: "5px 5px 0 0",
-        },
-        "& .Mui-disabled": {
-            color: "#FFFFFF",
-            opacity: 0.6,
-        },
-        "& .Mui-disabled .MuiOutlinedInput-notchedOutline": {
-            border: "1px solid #484850",
-        },
+  quantityRoot: {
+    color: "#FFFFFF",
+    backgroundColor: "#303039",
+    opacity: 0.6,
+    borderRadius: "5px",
+    "&:hover": {
+      backgroundColor: "#1E1E24",
+      borderRadius: "5px",
+      opacity: 1,
     },
-    selectRoot: {
-        color: "#FFFFFF",
+    "&:focus-within": {
+      backgroundColor: "#1E1E24",
+      borderRadius: "5px",
+      opacity: 1,
     },
-    icon: {
-        color: "#FFFFFF",
+    "& .MuiOutlinedInput-notchedOutline": {
+      border: "1px solid #484850",
     },
-    selectPaper: {
-        backgroundColor: "#1E1E24",
-        border: "1px solid #484850",
-        borderRadius: "5px",
-        color: "#FFFFFF",
-        "& li:hover": {
-            backgroundColor: "#303039",
-        },
+    "&:hover .MuiOutlinedInput-notchedOutline": {
+      border: "1px solid #484850",
     },
+    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      border: "1px solid #484850",
+      borderRadius: "5px 5px 0 0",
+    },
+    "& .Mui-disabled": {
+      color: "#FFFFFF",
+      opacity: 0.6,
+    },
+    "& .Mui-disabled .MuiOutlinedInput-notchedOutline": {
+      border: "1px solid #484850",
+    },
+  },
+  selectRoot: {
+    color: "#FFFFFF",
+  },
+  icon: {
+    color: "#FFFFFF",
+  },
+  selectPaper: {
+    backgroundColor: "#1E1E24",
+    border: "1px solid #484850",
+    borderRadius: "5px",
+    color: "#FFFFFF",
+    "& li:hover": {
+      backgroundColor: "#303039",
+    },
+  },
 }));
 
 function Post({
-    timePosted,
-    likes,
-    liked,
-    uri,
-    url,
-    updateLikes,
-    location,
-    spotifyLike,
-    spotifyId,
-    allPlaylists,
-    token,
+  timePosted,
+  likes,
+  liked,
+  uri,
+  url,
+  updateLikes,
+  location,
+  // spotifyLike,
+  spotifyId,
+  allPlaylists,
+  token,
+  // isClicked,
 }) {
-    // first part of location message based on post time
-    let message = "< 1 hour ago";
-    if (timePosted) {
-        if (timePosted < 2) {
-            message = "1 hour ago";
-        } else if (timePosted < 24) {
-            message = `${timePosted} hours ago`;
-        } else if (timePosted < 25) {
-            message = "1 day ago";
-        } else {
-            message = `${Math.ceil(parseInt(timePosted) / 24)} days ago`;
-        }
+  // first part of location message based on post time
+  let message = "< 1 hour ago";
+  if (timePosted) {
+    if (timePosted < 2) {
+      message = "1 hour ago";
+    } else if (timePosted < 24) {
+      message = `${timePosted} hours ago`;
+    } else if (timePosted < 25) {
+      message = "1 day ago";
+    } else {
+      message = `${Math.ceil(parseInt(timePosted) / 24)} days ago`;
     }
+  }
 
-    const [selectedPlaylist, setSelectedPlaylist] = useState("");
+  const [selectedPlaylist, setSelectedPlaylist] = useState("");
 
-    const handleChange = (e) => {
-        setSelectedPlaylist(e.target.value);
-        console.log(selectedPlaylist);
+  const handleChange = (e) => {
+    setSelectedPlaylist(e.target.value);
+    console.log(selectedPlaylist);
+  };
+
+  useEffect(() => {
+    // console.log("should be in here");
+    addToPlaylist();
+  }, [selectedPlaylist]);
+
+  async function addToPlaylist() {
+    // takes care of undefined id on first render of useEffect
+    if (selectedPlaylist !== "") {
+      const id = findPlaylistId();
+      const data = {
+        uris: [uri],
+        // position: 0,
+      };
+      console.log("data: ", data);
+      console.log("id: ", id);
+      try {
+        const response = await axios.post(
+          `https://api.spotify.com/v1/playlists/${id}/tracks`,
+          data,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        console.log("response: ", response);
+        return response;
+      } catch (error) {
+        console.log("error message: ", error.message);
+        return false;
+      }
+    }
+  }
+
+  function findPlaylistId() {
+    // console.log(allPlaylists);
+
+    for (let i = 0; i < allPlaylists.length; i++) {
+      let playlist = allPlaylists.find(
+        (item) => item.name === selectedPlaylist
+      );
+      if (playlist) return playlist.id;
+    }
+  }
+
+  const [spotifyLikeStatus, setSpotifyLikeStatus] = useState(false);
+
+  useEffect(() => {
+    if (token === undefined) {
+      return;
+    }
+    getSpotifyLikeStatus();
+  }, [token]);
+
+  async function getSpotifyLikeStatus() {
+    if (spotifyId === undefined) {
+      return;
+    }
+    const queryparam = `ids=${spotifyId}`;
+    try {
+      const status = await axios.get(
+        "https://api.spotify.com/v1/me/tracks/contains?" + queryparam,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setSpotifyLikeStatus(status.data[0]);
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async function spotifyLike() {
+    const data = {
+      ids: [spotifyId],
     };
-
-    useEffect(() => {
-        // console.log("should be in here");
-        addToPlaylist();
-    }, [selectedPlaylist]);
-
-    async function addToPlaylist() {
-        // takes care of undefined id on first render of useEffect
-        if (selectedPlaylist !== "") {
-            const id = findPlaylistId();
-            const data = {
-                uris: [uri],
-                // position: 0,
-            };
-            console.log("data: ", data);
-            console.log("id: ", id);
-            try {
-                const response = await axios.post(
-                    `https://api.spotify.com/v1/playlists/${id}/tracks`,
-                    data,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                            "Content-Type": "application/json",
-                        },
-                    }
-                );
-                console.log("response: ", response);
-                return response;
-            } catch (error) {
-                console.log("error message: ", error.message);
-                return false;
-            }
+    try {
+      const response = await axios.put(
+        "https://api.spotify.com/v1/me/tracks",
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
         }
+      );
+      setSpotifyLikeStatus(true);
+      return response;
+    } catch (error) {
+      return false;
     }
+  }
 
-    function findPlaylistId() {
-        // console.log(allPlaylists);
-
-        for (let i = 0; i < allPlaylists.length; i++) {
-            let playlist = allPlaylists.find((item) => item.name === selectedPlaylist);
-            if (playlist) return playlist.id;
-        }
-    }
-
-    const [spotifyLikeStatus, setSpotifyLikeStatus] = useState();
-
-    useEffect(() => {
-        if (token === undefined) {
-            return;
-        }
-        getSpotifyLikeStatus();
-    }, [token]);
-
-    async function getSpotifyLikeStatus() {
-        if (spotifyId === undefined) {
-            return;
-        }
-        const queryparam = `ids=${spotifyId}`;
-        try {
-            const status = await axios.get(
-                "https://api.spotify.com/v1/me/tracks/contains?" + queryparam, 
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-            setSpotifyLikeStatus(status.data[0]);
-        } catch (error) {
-            return false;
-        }
-    }
-
-    return (
-        <div className="card">
-            <div className="spotify-div">
-                <Spotify wide allowtransparency="false" height="118px" link={url} />
-                <div className="spotify-actions">
-                    {/* <button className="webplayer-buttons" onClick={spotifyLike}>
+  return (
+    <div className="card">
+      <div className="spotify-div">
+        <Spotify wide allowtransparency="false" height="118px" link={url} />
+        <div className="spotify-actions">
+          {/* <button className="webplayer-buttons" onClick={spotifyLike}>
                         <BsBookmarkHeart />
                         {spotifyLikeStatus ===}
                         {'Save to "Liked Songs"'}
                     </button> */}
 
-                    {spotifyLikeStatus === false ? (
-                        <button className="webplayer-buttons" onClick={spotifyLike}>
-                            <BsBookmarkHeart />
-                            {'Save to "Liked Songs"'}
-                        </button>
-                    ) : (
-                        <button className="webplayer-buttons" onClick={spotifyLike}>
-                            <BsBookmarkHeartFill />
-                            {'Saved to "Liked Songs"'}
-                        </button>
-                    )}
-                    <Popup
-                        // closeOnDocumentClick
-                        modal
-                        nested
-                        trigger={
-                            <button className="webplayer-buttons">
-                                {" "}
-                                <RiPlayListFill />
-                                {"Add to playlist"}
-                            </button>
-                        }
+          {spotifyLikeStatus === false ? (
+            <button className="webplayer-buttons" onClick={spotifyLike}>
+              <BsBookmarkHeart />
+              {'Save to "Liked Songs"'}
+            </button>
+          ) : (
+            <button className="webplayer-buttons">
+              <BsBookmarkHeartFill />
+              {'Saved to "Liked Songs"'}
+            </button>
+          )}
+          <Popup
+            // closeOnDocumentClick
+            modal
+            nested
+            trigger={
+              <button className="webplayer-buttons">
+                {" "}
+                <RiPlayListFill />
+                {"Add to playlist"}
+              </button>
+            }
+          >
+            {(close) => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="content">
+                  <FormControl sx={{ m: 1, width: "100%" }}>
+                    <InputLabel id="demo-multiple-name-label">
+                      Choose a playlist...
+                    </InputLabel>
+                    <Select
+                      labelId="demo-multiple-name-label"
+                      id="demo-multiple-name"
+                      value={selectedPlaylist}
+                      onChange={handleChange}
+                      input={<OutlinedInput label="Name" />}
+                      MenuProps={MenuProps}
                     >
-                        {(close) => (
-                            <div className="modal">
-                                <button className="close" onClick={close}>
-                                    &times;
-                                </button>
-                                <div className="content">
-                                    <FormControl sx={{ m: 1, width: "100%" }}>
-                                        <InputLabel id="demo-multiple-name-label">
-                                            Choose a playlist...
-                                        </InputLabel>
-                                        <Select
-                                            labelId="demo-multiple-name-label"
-                                            id="demo-multiple-name"
-                                            value={selectedPlaylist}
-                                            onChange={handleChange}
-                                            input={<OutlinedInput label="Name" />}
-                                            MenuProps={MenuProps}
-                                        >
-                                            {allPlaylists &&
-                                                allPlaylists.map((playlist, index) => (
-                                                    <MenuItem
-                                                        key={index}
-                                                        value={playlist.name}
-                                                    >
-                                                        {playlist.name}
-                                                    </MenuItem>
-                                                ))}
-                                        </Select>
-                                    </FormControl>
-                                </div>
-                            </div>
-                        )}
-                    </Popup>
+                      {allPlaylists &&
+                        allPlaylists.map((playlist, index) => (
+                          <MenuItem key={index} value={playlist.name}>
+                            {playlist.name}
+                          </MenuItem>
+                        ))}
+                    </Select>
+                  </FormControl>
                 </div>
-            </div>
-            <div className="action">
-                <p className="time">{message}</p>
-                <p className="location">{location}</p>
-                {liked === false ? (
-                    <button
-                        className="likes"
-                        onClick={updateLikes}
-                        style={{ color: "black", backgroundColor: "rgb(236, 236, 236)" }}
-                    >
-                        <FaRegHeart />
-                        {likes}
-                    </button>
-                ) : (
-                    <button
-                        className="likes"
-                        onClick={updateLikes}
-                        style={{ color: "#0065B8", backgroundColor: "#DCEFFE" }}
-                    >
-                        <FaHeart /> {likes}
-                    </button>
-                )}
-            </div>
+              </div>
+            )}
+          </Popup>
         </div>
-    );
+      </div>
+      <div className="action">
+        <p className="time">{message}</p>
+        <p className="location">{location}</p>
+        {liked === false ? (
+          <button
+            className="likes"
+            onClick={updateLikes}
+            style={{ color: "black", backgroundColor: "rgb(236, 236, 236)" }}
+          >
+            <FaRegHeart />
+            {likes}
+          </button>
+        ) : (
+          <button
+            className="likes"
+            onClick={updateLikes}
+            style={{ color: "#0065B8", backgroundColor: "#DCEFFE" }}
+          >
+            <FaHeart /> {likes}
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Post;
