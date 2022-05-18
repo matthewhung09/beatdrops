86aace30355a1b5840f07e0a26e38629e24d7e71
Griffin Scotti <griffinscotti@gmail.com>
2022-05-18 11:59:59 -0700

    wrote hashing function to group close posts

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -7,8 +7,26 @@ const musicnote = new Icon({
   iconSize: [75, 90],
 });
 
+//groups near post
+//returns a hashmap ([lat, long], [songs])
+function groupPosts(posts) {
+  let notes = new Map();
+  for (let i = 0; i < posts.length; i++) {
+    console.log(posts[i]);
+    let rLat = Math.ceil(posts[i].location.lat / 0.00002) * 0.000002;
+    let rLong = Math.ceil(posts[i].location.long / 0.00002) * 0.000002;
+    if (notes.has([rLat, rLong])) {
+      //let songs = notes.get([rLat, rLong]).push({ title: posts[i].title, artist: posts[i].artist });
+      notes.set([rLat, rLong], [{ title: posts[i].title, artist: posts[i].artist }]);
+    } else {
+      notes.set([rLat, rLong], [{ title: posts[i].title, artist: posts[i].artist }]);
+    }
+  }
+  return notes;
+}
 
 function Map({ lat, long, posts }) {
+  //const notes = groupPosts(posts);
   return (
     <MapContainer
       className="map"
