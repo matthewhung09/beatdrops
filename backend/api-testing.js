df8b398a65550cae4c3561deabfd2436674880d2
naomidonato <nstudentd@mgail.com>
2022-05-20 15:14:51 -0700

    api-testing

---
+++ backend/api-testing.js
@@ -0,0 +1,12 @@
+/*
+Feature: Post song to homepage
+As a student, I want to be able to see my post on the homepage so that I can be able to share the song I am currently listening to. 
+Scenario: Successful song post.
+	GIVEN I have chosen to post a song to the homepage.
+	WHEN I spell the song and title correctly, and the song exists on Spotify.
+	THEN I am able to see my song post on the homepage.
+Scenario: Unsuccessful song post.
+GIVEN I have chosen to post a song.
+	WHEN I spell a song name incorrectly. 
+	THEN I get an error message telling me that I spelled the song incorrectly.
+*/
