df5c3039644de243a90abe3f01af02ba49ed8955
Matthew Hung <mafewh119@gmail.com>
2022-05-23 15:33:33 -0700

    finished api testing

--- backend/backend.js
+++ backend/backend.js
@@ -90,6 +90,19 @@ app.get("/posts", checkUser, async (req, res) => {
   }
 });
 
+// only used for cypress testing
+app.get("/posts-test", async (req, res) => {
+  const lat = parseFloat(req.query.lat);
+  const long = parseFloat(req.query.long);
+  try {
+    const posts = await postServices.getPostsByLocation(lat, long);
+    res.status(201).json({ posts: posts, user: req.user });
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log(error);
+  }
+});
+
 // Creates a new post and adds it to the database
 app.post("/create", async (req, res) => {
   const new_post = await limiter.schedule(() =>
